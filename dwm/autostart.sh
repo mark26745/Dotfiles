d52ddc1c9b53772ed9bb4b4a4b478412d3bb5a01
mark26745 <72761972+mark26745@users.noreply.github.com>
2022-04-16 16:58:02 +0300

    First commit

---
+++ dwm/autostart.sh
@@ -0,0 +1,22 @@
+
+#! /bin bash
+#dwm autostart
+
+#compositor
+picom &
+
+#wallpaper --figure out random wallpapers
+nitrogen --restore &
+
+#bar info
+slstatus &
+
+#notifications
+dunst &
+
+#keychord manager
+sxhkd &
+
+
+
+
